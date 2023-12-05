bdf0e9537e75138816dfe6f9f5102e28cbe7f1d7
James <jimmykerr2003@gmail.com>
2023-12-05 11:00:56 +0000

    update script.js

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ person.fav_food = "Pizza";
 console.log(person.fav_food);
 
 const car = { make: "Jaguar", model: "Old", colour: "Maroon" };
+console.log(
+  `Have you been looking for a new way to travel? then introducing: ${car.make} ${car.model} featuring a record low greenhouse emission rate, heated door handles, auto-pilot that will always choose to kill pedestrians, and even a blinding ${car.colour} paintjob`
+);
