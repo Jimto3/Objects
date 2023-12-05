1b7775cebe4d5c176e906f319b3b19d6ac484fe4
James <jimmykerr2003@gmail.com>
2023-12-05 12:02:39 +0000

    update js

--- script.js
+++ script.js
@@ -6,7 +6,40 @@ console.log(`Age: ${person.age}`);
 person.fav_food = "Pizza";
 console.log(person.fav_food);
 
-const car = { make: "Jaguar", model: "Old", colour: "Maroon" };
-console.log(
-  `Have you been looking for a new way to travel? then introducing: ${car.make} ${car.model} featuring a record low greenhouse emission rate, heated door handles, auto-pilot that will always choose to kill pedestrians, and even a blinding ${car.colour} paintjob`
-);
+const person2 = {
+  name: "guy",
+  age: 29,
+  fav_colour: "Blue",
+  hello: function (name) {
+    console.log(`Hello ${name}!`);
+    return this.age;
+  },
+};
+let age = person2.hello("Gamer");
+console.log(age);
+
+const car = {
+  make: "Jaguar",
+  model: "Old",
+  colour: "chrome",
+  advert: function () {
+    console.log(
+      `Have you been looking for a new way to travel? then introducing: ${this.make} ${this.model} featuring a record low greenhouse emission rate, heated door handles, auto-pilot that will always choose to hit pedestrians, and even a blinding ${this.colour} paintjob <br>`
+    );
+  },
+};
+
+car.advert();
+
+const book = {
+  title: "Harry Potter",
+  author: "J.K Rowling",
+  pages: "too many",
+  advert: function () {
+    console.log(
+      `Introducing an all kind of new book called: ${book.title} by ${book.author} which has a staggering ${book.pages} pages!`
+    );
+  },
+};
+
+book.advert();
