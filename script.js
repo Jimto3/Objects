b676f19a74ba3f1ee14372bcb8d8605829d73c5c
James <jimmykerr2003@gmail.com>
2023-12-05 10:19:42 +0000

    Init Bruv

---
+++ script.js
@@ -0,0 +1,9 @@
+const person = { name: "Bob", age: "Ancient", fav_colour: "Red" };
+console.log(person);
+console.table(person);
+console.log(`Name: ${person.name}`);
+console.log(`Age: ${person.age}`);
+person.fav_food = "Pizza";
+console.log(person.fav_food);
+
+const car = { make: "Jaguar", model: "Old", colour: "Maroon" };
